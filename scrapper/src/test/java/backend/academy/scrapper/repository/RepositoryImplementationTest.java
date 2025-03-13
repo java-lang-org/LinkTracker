38cba2d29b4f87db5ab9cb21ad57cbe35236882b
Konstantin Vikhlyantsev <vihlancevk@gmail.com>
2025-03-13 18:42:42 +0300

    Add test for ChatRepository

--- scrapper/src/test/java/backend/academy/scrapper/repository/RepositoryImplementationTest.java
+++ scrapper/src/test/java/backend/academy/scrapper/repository/RepositoryImplementationTest.java
@@ -2,17 +2,24 @@ package backend.academy.scrapper.repository;
 
 import backend.academy.scrapper.ScrapperConfig;
 import backend.academy.scrapper.TestcontainersConfiguration;
+import backend.academy.scrapper.repository.impl.OrmChatRepository;
+import backend.academy.scrapper.repository.impl.OrmLinkRepository;
+import backend.academy.scrapper.repository.impl.SqlChatRepository;
+import backend.academy.scrapper.repository.impl.SqlLinkRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class RepositoryImplementationTest {
     @Autowired
     private ScrapperConfig scrapperConfig;
+
+    @Autowired
+    private ChatRepository chatRepository;
 
     @Autowired
     private LinkRepository linkRepository;
@@ -22,13 +29,16 @@ class RepositoryImplementationTest {
         // Arrange
         ScrapperConfig.DataBase.AccessType accessType = scrapperConfig.dataBase().accessType();
 
-        // Act
-        String linkRepositoryClassName = linkRepository.getClass().getSimpleName();
-
-        // Assert
+        // Act & Assert
         switch (accessType) {
-            case SQL -> assertTrue(linkRepositoryClassName.startsWith("SqlLinkRepository"));
-            case ORM -> assertTrue(linkRepositoryClassName.startsWith("OrmLinkRepository"));
+            case SQL -> {
+                assertThat(chatRepository instanceof SqlChatRepository).isTrue();
+                assertThat(linkRepository instanceof SqlLinkRepository).isTrue();
+            }
+            case ORM -> {
+                assertThat(chatRepository instanceof OrmChatRepository).isTrue();
+                assertThat(linkRepository instanceof OrmLinkRepository).isTrue();
+            }
         }
     }
 }
